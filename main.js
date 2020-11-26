8d80f9ec97f50cde3099d6689b870f66f71d5f08
brian926 <brian.antunes@yahoo.com>
2020-11-25 21:59:15 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+var roleHarvester = require('role.harvester');
+var roleUpgrader = require('role.upgrader')
+
+module.exports.loop = function() {
+
+    for (let name in Game.creeps){
+        var creep = Game.creeps[name];
+
+        if (creep.memory.role == 'harvester'){
+            roleHarvester.run(creep);
+        }
+        else if (creep.memory.role == 'upgrader'){
+            roleUpgrader.run(creep);
+        }
+    }
+};
